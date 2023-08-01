b22241588036011cad7c257198c687a231e6b783
nandureigns <139434750+nandureigns@users.noreply.github.com>
2023-08-01 21:44:51 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: KOYEB
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- SHProSearchbot-AutoFilterBot is a open source project. 
+- nk_moviezzz-AutoFilterBot is a open source project. 
 - Source - 🔐 
 
 <b>DEVS:</b>

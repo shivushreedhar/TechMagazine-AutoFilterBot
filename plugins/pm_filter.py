335d27075b00dee251848f8291391361ae71e3aa
nandureigns <139434750+nandureigns@users.noreply.github.com>
2023-08-04 14:28:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -28,7 +28,11 @@ logger.setLevel(logging.ERROR)
 
 BUTTONS = {}
 SPELL_CHECK = {}
-
+@Client.on_message(filters.private & filters.text & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.private & filters.text & filters.incoming)
+async def pv_filter(client, message):
+    rk = await global_filters(client, message)
+    if rk == False:
+        await auto_filter(client, message)
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
